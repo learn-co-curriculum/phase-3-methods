8584bdb9e5e9979133e983c919d68a43c69f494d
ihollander <ianholla@gmail.com>
2021-07-04 11:57:54 -0400

    Solution

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+  num1 + num2
+end
+
+def halve(num)
+  return nil unless num.class == Integer
+  
+  num / 2
+end
