402c21773811211a40774b6d072788c2592c9d9f
ihollander <ianholla@gmail.com>
2021-07-09 10:29:56 -0400

    Update method syntax for consistency

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,9 +1,9 @@
 require_relative 'spec_helper'
 require_relative '../methods.rb'
 
-describe 'methods' do
+describe 'methods.rb' do
 
-  describe 'greet_programmer' do
+  describe '#greet_programmer' do
   
     it 'outputs the string "Hello, programmer!"' do
       expect { greet_programmer }.to output(a_string_including("Hello, programmer!")).to_stdout
@@ -11,7 +11,7 @@ describe 'methods' do
     
   end
   
-  describe 'greet' do
+  describe '#greet' do
   
     it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
       expect { greet("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
@@ -23,7 +23,7 @@ describe 'methods' do
   
   end
   
-  describe 'greet_with_default' do
+  describe '#greet_with_default' do
   
     it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
       expect { greet_with_default("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
@@ -35,7 +35,7 @@ describe 'methods' do
   
   end
   
-  describe 'add' do
+  describe '#add' do
   
     it 'returns the sum of two numbers' do
       expect(add(2, 5)).to be(7)
@@ -43,7 +43,7 @@ describe 'methods' do
   
   end
 
-  describe 'halve' do
+  describe '#halve' do
     
     it 'returns half of the given integer' do
       expect(halve(6)).to be(3)
