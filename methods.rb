1a31099f53ea35ac123d6fd08a4c8df0f03bcb0f
dialloflatno <brahimdiallo23@gmail.com>
2021-11-23 12:49:25 -0500

    commit tree 1bc44eac3de71271ca1fece7031a37e0e777ba68

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
-# Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello,#{name}!"
+end
+
+def greet_with_default(name = Jacqk)
+    puts "Hello,#{name}!"
+end
+
+def add
+   return  1+1
+end
+
+def halve(2)
+    return 2/2
+end
+
+
