e35051eb39585d762c504fefb8a5991704243489
Tosie <minnaji@outlook.com>
2022-04-28 11:11:19 -0400

    commit tree 039fbf9b1e592a7d96bc3f94ea920696c2431338

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet (name)
+    puts  "Hello, #{name}!"
+end 
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add num1,num2
+    num1 + num2
+end
+
+def halve num
+    (num_i)/2
+end
