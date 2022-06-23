0e44007caf628235fcdd7872fafe0fc7f760aebc
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-06-23 07:56:54 +0300

    commit tree a259c3e549fdef99863e7c724cdd4fa4c7adc6a3

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1,num2
+    num1 + num2
+end
+
+def halve number
+    if number.class == Integer
+        return number / 2
+    else 
+        return nil
+    end
+end
