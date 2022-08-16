51f557ee7849dc3aa6b78894e3ab92b75efc5a11
simon waronja <simon.waronja@student.moringaschool.com>
2022-08-16 16:27:00 +0300

    commit tree da250a67f81d30fdec6ca18fd4b390abd40971b4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+   puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name="Naureen")
+    puts "Hello, #{name}!"
+    puts "Hello, programmer!"
+end
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+
+def halve(num)
+    if num.class == Integer
+        return num/2
+    end
+
+    return nil
+end
