61bcddca136f3863366055aa7cb0d8e7789f14bf
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-08-17 10:45:18 +0300

    commit tree aa170f9fca25f0aac6e5d7dab4363fb8bcc05438

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = 'programmer')
+    puts "Hello, #{name}!"
+end
+
+def add (num1, num2)
+    num1 + num2
+end
+
+def halve (num1)
+    return nil unless num1.class == Integer
+
+    num1 / 2
+end
