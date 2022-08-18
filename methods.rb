5e0e3133d7524c08461f5f6c8c7338a0d2d72985
Milton Ouma <ouma.nyang1@gmail.com>
2022-08-18 09:01:37 +0300

    commit tree 72f0c01be3d7856a036e3f6e3c86d7247bbd9312

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+
+#Greet programmer
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve number
+    if number.class != Integer
+        return nil
+    end
+    number / 2
+end
