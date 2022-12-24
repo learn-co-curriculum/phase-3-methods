695c2cc8f06c6429a2348c760732c47bbf22325b
Subtext Pro <hello@subtext.pro>
2022-12-23 19:20:59 -0500

    commit tree 6f07c09680160262103d12cbbdaff13f3d52c8c4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+
+def my_method(param)
+    puts 'Running my_method'
+    param + 1
+end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    num / 2
+end
