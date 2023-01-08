d9046433e0ba287f2bf5770f00f06132e73d6de8
+ <rodarte_sarah@yahoo.com>
2023-01-07 23:42:34 -0700

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+
+def add num1, num2 
+    num1 + num2
+end
+
+result = add(2, 2)
+
+def halve(number)
+    if number.class == String
+        return nil
+    end
+    number / 2
+    
+end
+halve(6)
+halve("six")
+
+
+
