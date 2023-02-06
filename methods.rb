c4e410ebc8466c7f990a313bc9014eae6e01afec
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-06 11:26:33 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
-# Your code here!
+#greet_programmer
+def greet_programmer
+   puts "Hello, programmer!"
+end    
+greet_programmer
+
+#greet
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+    # puts "Hello, Jimmy!"
+end    
+greet "Jimmy"
+
+# greet_with_default
+def greet_with_default(user = "programmer")
+    puts "Hello, #{user}!"
+end
+greet_with_default "Naureen"
+
+#add
+def add num1, num2
+    puts num1
+    puts num2
+    num1 + num2
+end 
+
+# halve
+def halve(num)
+    return nil unless num.is_a?(Integer)
+    puts num 
+    num /2
+end 
+ 
