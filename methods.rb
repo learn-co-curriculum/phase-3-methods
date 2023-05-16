b22fb37746fa58a2a124844a9424b9668eaa96db
jeremykiokoo <jeremy.kioko@student.moringaschool.com>
2023-05-16 12:40:18 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name ="programmer")
+    puts "Hello, #{name}!"
+end
+
+
+def add num1, num2
+    num1 + num2
+end
+
+
+def halve(number)
+    return nil unless number.is_a?(Numeric)
+  
+    number / 2.0
+  end
